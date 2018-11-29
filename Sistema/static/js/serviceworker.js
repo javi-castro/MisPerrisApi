b9d2e0b1a6459171a3d97c45d500a67562718725
javi castro <anth.castro@alumnos.duoc.cl>
2018-11-29 03:49:24 -0300

    ya

--- Sistema/static/js/serviceworker.js
+++ Sistema/static/js/serviceworker.js
@@ -16,19 +16,6 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('activate', event => {
-  var keepList = [CACHE_NAME];
-
-  event.waitUntil(
-      caches.keys().then(cacheNameList => {
-          return Promise.all(cacheNameList.map(cacheName => { 
-              if (keepList.indexOf(cacheName) === -1) {
-                  return caches.delete(cacheName);
-              }
-          }));
-      })
-  );
-});
 
 self.addEventListener('fetch', function(event) {
   var requestUrl = new URL(event.request.url);
